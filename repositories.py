7674d3ad70efab19ecd80eaa54c80a6ea3d52237
Sielski, Bartosz (GlobalLogic) <bartosz.sielski@globallogic.com>
2022-07-17 15:27:28 +0200

    Home_budget- cost listing,income listing

--- repositories.py
+++ repositories.py
@@ -2,8 +2,40 @@ import sqlite3
 
 
 class EntryRepository:
+    def get_costs(self):
+        with sqlite3.connect('database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("""SELECT 
+            entry.id, 
+            entry.name,
+            entry.created_at, 
+            entry.amount, 
+            category.name  
+            FROM 
+                entry 
+            LEFT JOIN category ON entry.category_id  = category.id 
+            WHERE entry.amount < 0 
+            ORDER BY created_at DESC;""")
+            return cursor.fetchall()
+
+    def get_incomes(self):
+        with sqlite3.connect('database.db') as connection:
+            cursor = connection.cursor()
+            cursor.execute("""SELECT 
+            entry.id, 
+            entry.name,
+            entry.created_at, 
+            entry.amount, 
+            category.name  
+            FROM 
+                entry 
+            LEFT JOIN category ON entry.category_id  = category.id 
+            WHERE entry.amount > 0 
+            ORDER BY created_at DESC;""")
+            return cursor.fetchall()
+
     def save(self, name, category_id, amount):
-        with sqlite3.connect('my_db.db') as connection:
+        with sqlite3.connect('database.db') as connection:
             cursor = connection.cursor()
             cursor.execute('INSERT INTO entry(`category_id`, `name`, `amount`) VALUES(?,?,?)',(
                            category_id,
@@ -14,7 +46,7 @@ class EntryRepository:
 
 class CategoryRepository:
     def get_by_name(self, name):
-        with sqlite3.connect('my_db.db') as connection:
+        with sqlite3.connect('database.db') as connection:
             cursor = connection.cursor()
             cursor.execute('SELECT `id`, `name` FROM category WHERE `name`=?', (name,))
             return cursor.fetchone()
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from terminaltables import AsciiTable
 
 
 class AbstractView(ABC):
@@ -13,19 +14,30 @@ class AbstractView(ABC):
         self.repositories[name] = repository
 
 
-class AddCost(AbstractView):
+class AddView:
+    def get_category_id(self, repository):
+        found_category = False
+        while not found_category:
+            try:
+                category_name = input('Kategoria')
+                category_id, _ = repository.get_by_name(category_name)
+                found_category = True
+            except TypeError:
+                found_category = False
+
+        return category_id
+
+
+class AddCost(AbstractView, AddView):
     SHORTCUT = 'dk'
     LABEL = 'Dodaj koszt'
 
     def draw(self):
         print(AddCost.LABEL)
         name = input('Tytul')
-        category_name = input('Kategoria')
         amount = float(input('Wartosc'))
-
-        category_id, name = self.repositories['category'].get_by_name(category_name)
+        category_id = self.get_category_id(self.repositories['category'])
         self.repositories['entry'].save(name, category_id, amount * -1)
-
 
 class ListCosts(AbstractView):
     SHORTCUT = 'wk'
@@ -33,14 +45,25 @@ class ListCosts(AbstractView):
 
     def draw(self):
         print(ListCosts.LABEL)
+        rows = [
+            ['tytuł', 'data dodania', 'kwota', 'kategoria']
+        ]
+        for cost_id, name, created_at, amount, category in self.repositories['entry'].get_costs():
+            rows.append([name, created_at, amount, category])
 
+        table = AsciiTable(rows)
+        print(table.table)
 
-class AddIncome(AbstractView):
+class AddIncome(AbstractView, AddView):
     SHORTCUT = 'dp'
     LABEL = 'Dodaj przychody'
 
     def draw(self):
         print(AddIncome.LABEL)
+        name = input('Tytul')
+        amount = float(input('Wartosc'))
+        category_id = self.get_category_id(self.repositories['category'])
+        self.repositories['entry'].save(name, category_id, amount)
 
 
 class ListIncomes(AbstractView):
@@ -49,6 +72,14 @@ class ListIncomes(AbstractView):
 
     def draw(self):
         print(ListIncomes.LABEL)
+        rows = [
+            ['tytuł', 'data dodania', 'kwota', 'kategoria']
+        ]
+        for cost_id, name, created_at, amount, category in self.repositories['entry'].get_incomes():
+            rows.append([name, created_at, amount, category])
+
+        table = AsciiTable(rows)
+        print(table.table)
 
 
 class MainMenu:
@@ -70,5 +101,3 @@ class MainMenu:
         print('Co zrobic: ')
         for shortcut, screen in MainMenu.OPTIONS.items():
             print(f'[{shortcut}] - {screen.LABEL}')
-
-
